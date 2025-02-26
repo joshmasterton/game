749fbbed12aedb65ce4b1de848d806900f41eff1
joshmasterton <joshmasterton@tuta.io>
2025-02-26 01:35:41 +0000

    Update client socket connection settings

--- client/src/config/socket.config.ts
+++ client/src/config/socket.config.ts
@@ -2,4 +2,7 @@ import { io } from "socket.io-client";
 
 const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3000";
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  transports: ["websockets"],
+  withCredentials: true,
+});
